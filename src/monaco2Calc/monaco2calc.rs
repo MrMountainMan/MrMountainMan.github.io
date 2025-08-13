fc289875de7f669f0bfd2b291cc093fa44953404
MrMountainMan <jamesetpater@gmail.com>
2025-08-13 19:34:25 -0400

    completed version 1 of monaco 2 calc

--- src/monaco2Calc/monaco2calc.rs
+++ src/monaco2Calc/monaco2calc.rs
@@ -1,74 +1,93 @@
-use std::collections::HashMap;
-use dioxus_logger::tracing;
+use std::{collections::HashMap, ptr::null};
 use regex::Regex;
-use dioxus::prelude::*;
+use dioxus::{html::input, prelude::*};
 
-static HOURS_VALUE: GlobalSignal<String> = Global::new(|| "".to_string());
-static MINUTES_VALUE: GlobalSignal<String> = Global::new(|| "".to_string());
-static SECONDS_VALUE: GlobalSignal<String> = Global::new(|| "".to_string());
-static MILLISECONDS_VALUE: GlobalSignal<String> = Global::new(|| "".to_string());
-
-static HOURS_VALUE_OLD: GlobalSignal<String> = Global::new(|| "".to_string());
-static MINUTES_VALUE_OLD: GlobalSignal<String> = Global::new(|| "".to_string());
-static SECONDS_VALUE_OLD: GlobalSignal<String> = Global::new(|| "".to_string());
-static MILLISECONDS_VALUE_OLD: GlobalSignal<String> = Global::new(|| "".to_string());
-static COINS_OLD: GlobalSignal<String> = Global::new(|| "".to_string());
-
-static LEVEL: GlobalSignal<String> = Global::new(|| "yacht_club".to_string());
-static COINS: GlobalSignal<String> = Global::new(|| "".to_string());
-static PLAYER_MULT: GlobalSignal<String> = Global::new(|| "8".to_string());
-
-static SCORE: GlobalSignal<String> = Global::new(|| "".to_string());
-
-
-static MAX_COIN_AMOUNTS: GlobalSignal<HashMap<&str, u32>> = Global::new(|| HashMap::from([
-    ("yacht_club", 130),
-    ("hotel", 169),
-    ("bona_fide_sanitation", 270),
-    ("cocktail_party", 317),
-    ("prison", 231),
-    ("museum", 296),
-    ("shipyard", 362),
-    ("data_center", 300),
-    ("night_club", 399),
-    ("opera", 294),
-    ("bank", 403),
-    ("palace", 337), //unconfirmed
-    ("safehouse", 310), //unconfirmed
-    ("catacombs", 525), //unconfirmed
-    ("casino", 464), //unconfirmed
-    ("bonhomme", 589), //unconfirmed
-    ("petit_bank_optional", 315), //unconfirmed
-    ("museum_optional", 271), //unconfirmed
-    ("office_optional", 268), //unconfirmed
-    ("art_gallery_optional", 274), //unconfirmed
-]));
-
+use dioxus_logger::tracing;
 
 
 pub fn Monaco2CalcMain() -> Element
 {
-    let mut players = use_signal(|| 1);
+    let coins_per_level: Signal<HashMap<&str, u16>> = use_signal(|| HashMap::from([
+        ("yacht_club", 130),
+        ("hotel", 169),
+        ("bona_fide_sanitation", 270),
+        ("cocktail_party", 317),
+        ("prison", 231),
+        ("museum", 296),
+        ("shipyard", 362),
+        ("data_center", 300),
+        ("night_club", 399),
+        ("opera", 294),
+        ("bank", 403),
+        ("palace", 337), //unconfirmed
+        ("safehouse", 310), //unconfirmed
+        ("catacombs", 525), //unconfirmed
+        ("casino", 464), //unconfirmed
+        ("bonhomme", 589), //unconfirmed
+        ("petit_bank_optional", 315), //unconfirmed
+        ("museum_optional", 271), //unconfirmed
+        ("office_optional", 268), //unconfirmed
+        ("art_gallery_optional", 274), //unconfirmed
+    ]));
 
-rsx!
+    let mut level: Signal<String> = use_signal(|| "yacht_club".to_string());
+    let mut players: Signal<u16> = use_signal(|| 8);
+    let mut coins: Signal<u16> = use_signal(|| 0);
+
+    let mut hours: Signal<u16> = use_signal(|| 0);
+    let mut minutes: Signal<u16> = use_signal(|| 0);
+    let mut seconds: Signal<u16> = use_signal(|| 0);
+    let mut milliseconds: Signal<u16> = use_signal(|| 0);
+
+    rsx!
     {
+        //change the icon and the website title
         document::Link {rel: "icon", href: asset!("./assets/monaco2.ico")}
         document::Title{"Monaco 2 Score Calculator"}
-
-        //link { rel: "stylesheet", href: "/cgstylesheet.css" }
         div
-        {
+        {   
+            //display header
             h1 {"Monaco 2 Score Calculator"}
+            br {}
+            {LevelSelector(level)}
+            br {}
+            "Level selected is: {level}"
+            br {}
+            br {}
+            {PlayerSelector(players)}
+            br {}
+            "Player multiplier is: {players}"
+            br {}
+            br {}
+            {CoinsCollected(coins, level, coins_per_level)}"/{coins_per_level()[&level() as &str]}"
+            br {}
+            "Coins collected: {coins}"
+            br {}
+            br {}
+            {CompletionTime(hours, minutes, seconds, milliseconds)}
+            br {}
+            br {}
+            "Final score: {CalculateScore(level, players, coins, hours, minutes, seconds, milliseconds)}"
+
+
+
         }
-        div
-        {
-            //level
-            label { r#for: "level_name", "Level: " }
+
+
+    }
+}
+
+pub fn LevelSelector(mut level: Signal<String>) -> Element
+{
+    rsx!
+    {
+        label { r#for: "level_name", "Level: " }
             select
             {
                 name: "level",
                 id: "level_name",
-                oninput: move |event| { *LEVEL.write() = event.value(); },
+                value: level,
+                oninput: move |event| { *level.write() = event.value(); },
                 option { value: "yacht_club", "Yacht Club"}
                 option { value: "hotel", "Hotel"}
                 option { value: "bona_fide_sanitation", "Bona Fide Sanitation"}
@@ -90,37 +109,65 @@ rsx!
                 option { value: "office_optional", "Office (Optional)"}
                 option { value: "art_gallery_optional", "Art Gallery (Optional)"}
             }
+    }
+}
 
-            //number of players
-            br {}
-            br {}
-            label { r#for: "num_players", "Number of Players: " }
-            select
-            {
-                name: "players",
-                id: "num_players",
-                oninput: move |event| { players.set(event.value().parse().unwrap()); *PLAYER_MULT.write() = event.value(); },
-                option { value: 8, "1"}
-                option { value: 6, "2"}
-                option { value: 5, "3"}
-                option { value: 4, "4"}
-            }
+pub fn PlayerSelector(mut players: Signal<u16>) -> Element
+{
 
-            //completion time
-            br {}
-            br {}
-            "Time to complete: "
+    rsx!
+    {
+        label { r#for: "num_players", "Number of Players: " }
+        select
+        {
+            name: "players",
+            id: "num_players",
+            onchange: move |event| { *players.write() = event.value().parse().unwrap(); },
+            option { value: 8, "1"}
+            option { value: 6, "2"}
+            option { value: 5, "3"}
+            option { value: 4, "4"}
+            
+        }
+    }
+}
+
+pub fn CoinsCollected(mut coins: Signal<u16>, level: Signal<String>, coins_per_level: Signal<HashMap<&str, u16>>) -> Element
+{
+    rsx!
+    {
+        label { r#for: "coins_collected", "Coins Collected: " }
+        input {
+            id: "coins_collected",
+            r#type: "number",
+            min: 0,
+            max: coins_per_level()[&level() as &str],
+            value: coins,
+            oninput: move |event| { CheckValidInput(event, coins, coins_per_level()[&level() as &str]); },
+            onpaste: move |event| { event.prevent_default(); },
+            onkeypress: move |event| { PreventNonNumericalInput(event); },
+            onkeyup: move |_| { CheckOverflow(coins, coins_per_level()[&level() as &str]) },
+        }
+    }
+
+}
+
+
+pub fn CompletionTime(hours: Signal<u16>, minutes: Signal<u16>, seconds: Signal<u16>, milliseconds: Signal<u16>) -> Element
+{
+    rsx!
+    {
+        "Time to complete: "
             input {
                 id: "clear_time_hours",
                 r#type: "number",
                 min: 0,
                 max: 99,
-                value: HOURS_VALUE(),
-                placeholder: 0,
-                oninput: move |event| { *HOURS_VALUE.write() = event.value(); },
-                onkeyup: move |event| { checkOverflowValue(event, 99, "hours") },
-                onkeypress: move |event| { preventNonNumericalInput(event, 99, "hours"); },
+                value: hours,
+                oninput: move |event| { CheckValidInput(event, hours, 99); },
                 onpaste: move |event| { event.prevent_default(); },
+                onkeypress: move |event| { PreventNonNumericalInput(event); },
+                onkeyup: move |event| { CheckOverflow(hours, 99) },
             }
             ":"
             input {
@@ -128,11 +175,11 @@ rsx!
                 r#type: "number",
                 min: 0,
                 max: 59,
-                value: MINUTES_VALUE(),
-                placeholder: 0,
-                oninput: move |event| { *MINUTES_VALUE.write() = event.value(); },
-                onkeypress: move |event| { preventNonNumericalInput(event, 99, "minutes"); },
+                value: minutes,
+                oninput: move |event| { CheckValidInput(event, minutes, 59); },
                 onpaste: move |event| { event.prevent_default(); },
+                onkeypress: move |event| { PreventNonNumericalInput(event); },
+                onkeyup: move |event| { CheckOverflow(minutes, 59) },
             }
             ":"
             input {
@@ -140,11 +187,11 @@ rsx!
                 r#type: "number",
                 min: 0,
                 max: 59,
-                value: SECONDS_VALUE(),
-                placeholder: 0,
-                oninput: move |event| { *SECONDS_VALUE.write() = event.value(); },
-                onkeypress: move |event| { preventNonNumericalInput(event, 99, "seconds"); },
+                value: seconds,
+                oninput: move |event| { CheckValidInput(event, seconds, 59); },
                 onpaste: move |event| { event.prevent_default(); },
+                onkeypress: move |event| { PreventNonNumericalInput(event); },
+                onkeyup: move |event| { CheckOverflow(seconds, 59) },
             }
             "."
             input {
@@ -152,129 +199,81 @@ rsx!
                 r#type: "number",
                 min: 0,
                 max: 999,
-                value: MILLISECONDS_VALUE(),
-                placeholder: 0,
-                oninput: move |event| { *MILLISECONDS_VALUE.write() = event.value(); },
-                onkeypress: move |event| { preventNonNumericalInput(event, 999, "milliseconds"); },
+                value: milliseconds,
+                oninput: move |event| { CheckValidInput(event, milliseconds, 999); },
                 onpaste: move |event| { event.prevent_default(); },
+                onkeypress: move |event| { PreventNonNumericalInput(event); },
+                onkeyup: move |event| { CheckOverflow(milliseconds, 999) },
             }
-
-            br {}
-            br {}
-            label { r#for: "coins_collected", "Coins Collected: " }
-            input {
-                id: "coins_collected",
-                r#type: "number",
-                min: 0,
-                max: MAX_COIN_AMOUNTS()[&LEVEL() as &str],
-                value: COINS(),
-                placeholder: 0,
-                oninput: move |event| { *COINS.write() = event.value(); },
-                onkeypress: move |event| { preventNonNumericalInput(event, MAX_COIN_AMOUNTS()[&LEVEL() as &str], "coins"); },
-                onpaste: move |event| { event.prevent_default(); },
-            }
-
-
-            br {}
-            br {}
-            button { onclick: move |_| calculateScore(), "click this button" }
-
-            br {}
-            br {}
-            text { "Score: {SCORE}" }
-
-
-        }
-
     }
 
 }
 
-fn checkOverflowValue(event: Event<KeyboardData>, max_value: u32, unit: &str)
-{
-    tracing::info!("checking for overflow");
-
-    let parse_result;
-
-    match unit
-    {
-        "hours" => {
-            parse_result = HOURS_VALUE().clone().parse::<u32>();
-            match parse_result {
-                Ok(num) => { if num > max_value { *HOURS_VALUE.write() = HOURS_VALUE_OLD().clone() } },
-                Err(e) => { if !HOURS_VALUE().clone().is_empty() { *HOURS_VALUE.write() = HOURS_VALUE_OLD().clone() } },
-            }
-        },
-        "minutes" => *MINUTES_VALUE.write() = MINUTES_VALUE_OLD().clone(),
-        "seconds" => *SECONDS_VALUE.write() = SECONDS_VALUE_OLD().clone(),
-        "milliseconds" => *MILLISECONDS_VALUE.write() = MILLISECONDS_VALUE_OLD().clone(),
-        "coins" => *COINS.write() = COINS_OLD().clone(),
-        _ => {},
-    };
-
-}
-
-fn preventNonNumericalInput(event: Event<KeyboardData>, max_value: u32, unit: &str)
+pub fn PreventNonNumericalInput(event: Event<KeyboardData>)
 {
     let reg = Regex::new(r"[0-9]+").unwrap();
     if !reg.is_match(&(event.key().to_string()))
     {
         event.prevent_default();
-    }
-    else
-    {
-        //save the current value to the old value
-        match unit
-        {
-            "hours" => if !HOURS_VALUE().clone().is_empty() && HOURS_VALUE().parse::<u32>().unwrap() <= max_value {*HOURS_VALUE_OLD.write() = HOURS_VALUE().clone()} else { *HOURS_VALUE_OLD.write() = max_value.to_string()},
-            "minutes" => if MINUTES_VALUE().parse::<u32>().unwrap() <= max_value {*MINUTES_VALUE_OLD.write() = MINUTES_VALUE().clone()} else { *MINUTES_VALUE_OLD.write() = max_value.to_string()},
-            "seconds" => if SECONDS_VALUE().parse::<u32>().unwrap() <= max_value {*SECONDS_VALUE_OLD.write() = SECONDS_VALUE().clone()} else { *MINUTES_VALUE_OLD.write() = max_value.to_string()},
-            "milliseconds" => if MILLISECONDS_VALUE().parse::<u32>().unwrap() <= max_value {*MILLISECONDS_VALUE_OLD.write() = MILLISECONDS_VALUE().clone()} else { *MILLISECONDS_VALUE_OLD.write() = max_value.to_string()},
-            "coins" => if COINS().parse::<u32>().unwrap() <= max_value {*COINS_OLD.write() = COINS().clone()} else { *COINS_OLD.write() = max_value.to_string()},
-            _ => {},
-        };
-
-
-
-        /*
-        if format!("{}{}", event.key().to_string(), current_num).parse::<u32>().unwrap() > max_value
-        {
-            event.prevent_default();
-        }*/
+        return;
     }
 }
 
-fn calculateScore()//, hours_time: String, minutes_time: String, seconds_time: String, milliseconds_time: String)
+pub fn CheckOverflow(mut value: Signal<u16>, max_value: u16)
 {
-    //convert all the times to numbers
-    let mut hours: f32 = 0.0;
-    let mut minutes: f32 = 0.0;
-    let mut seconds: f32 = 0.0;
-    let mut milliseconds: f32 = 0.0;
-
-    if !HOURS_VALUE().is_empty()
+    if value() > max_value
     {
-        hours = HOURS_VALUE().parse::<f32>().unwrap();
+        *value.write() = max_value;
     }
+}
 
-    if !MINUTES_VALUE().is_empty()
+pub fn CheckValidInput(event: Event<FormData>, mut value: Signal<u16>, max_value: u16)
+{
+    if event.value().is_empty()
     {
-        minutes = MINUTES_VALUE().parse::<f32>().unwrap();
+        *value.write() = 0;
+        return;
     }
 
-    if !SECONDS_VALUE().is_empty()
+    if event.value().parse::<u16>().is_ok() && event.value().parse::<u16>().unwrap() <= max_value
     {
-        seconds = SECONDS_VALUE().parse::<f32>().unwrap();
+        *value.write() = event.value().parse().unwrap();
+        return;
+    }
+
+    *value.write() = value();
+
+}
+
+pub fn CalculateScore(level: Signal<String>, players: Signal<u16>, coins: Signal<u16>, hours: Signal<u16>, minutes: Signal<u16>, seconds: Signal<u16>, milliseconds: Signal<u16>) -> f64
+{
+    /*
+    let mut f_hours: f32 = 0.0;
+    let mut f_minutes: f32 = 0.0;
+    let mut f_seconds: f32 = 0.0;
+    let mut f_milliseconds: f32 = 0.0;
+
+    if !hours().is_empty()
+    {
+        f_hours = hours() as f32;
+    }
+
+    if !minutes().is_empty()
+    {
+        f_minutes = minutes() as f32;
+    }
+
+    if !seconds().is_empty()
+    {
+        f_seconds = seconds() as f32;
     }
     
-    if !MILLISECONDS_VALUE().is_empty()
+    if !milliseconds().is_empty()
     {
-        milliseconds = MILLISECONDS_VALUE().parse::<f32>().unwrap();
-    }
+        f_milliseconds = milliseconds() as f32;
+    }*/
     
-    let total_time: f32 = (hours * 3600f32) + (minutes * 60f32) + seconds + (milliseconds / 100f32);
-    
-    tracing::info!(total_time);
+    let total_time: f32 = ((hours() as f32 * 3600.0) + (minutes() as f32 * 60.0) + seconds() as f32 + (milliseconds() as f32/ 100.0)).into();
 
     let mut time_under_hour: f32 = 3600.0 - total_time;
     if time_under_hour < 0.0 
@@ -282,12 +281,11 @@ fn calculateScore()//, hours_time: String, minutes_time: String, seconds_time: S
         time_under_hour = 0.0;
     }
 
-    tracing::info!(time_under_hour);
+    let result: f64 = time_under_hour as f64 + coins() as f64 * players() as f64;
 
-    let result: f32 = time_under_hour + COINS().parse::<f32>().unwrap() * PLAYER_MULT().parse::<f32>().unwrap();
-
-    tracing::info!(result);
+    return result * 100.0;
 
 }
+
 
 
