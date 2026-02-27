c1fd6a8c2dd55901657f4d7c048bf6fdb14019fa
James Dickson <89423904+MrMountainMan@users.noreply.github.com>
2026-02-27 01:23:38 -0500

    put minimal actual site back

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,58 @@
 use dioxus::prelude::*;
+/*
+#[path = "./monaco2calc/monaco2calc.rs"]
+mod monaco2calc;
+use dioxus_logger::tracing::Level;
+use monaco2calc::Monaco2CalcMain;
 
-const FAVICON: Asset = asset!("/assets/favicon.ico");
-const MAIN_CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
+
+#[path = "./payday3stats/payday3stats.rs"]
+mod payday3stats;
+use payday3stats::Payday3Stats;
+*/
+#[derive(Debug, Clone, Routable, PartialEq)]
+#[rustfmt::skip]
+enum Route {
+    #[route("/")]
+    Home {},
+    /*
+    #[route("/monaco-2-score-calculator")]
+    Monaco2CalcMain {},
+    #[route("/payday-3-stats")]
+    Payday3Stats {},*/
+}
+
+//const DEFAULT_ICON: Asset = asset!("/assets/icon.ico");
+//const MAIN_CSS: Asset = asset!("/assets/main.css");
+
 
 fn main() {
+    //dioxus_web::launch::launch(App, vec![], dioxus_web::Config::new().hydrate(true));
+    //dioxus_logger::init(Level::INFO).expect("logger failed to init");
     dioxus::launch(App);
 }
+
 
 #[component]
 fn App() -> Element {
     rsx! {
-        document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS }
-        Hero {}
-
+        //document::Link { rel: "icon", href: DEFAULT_ICON }
+        //document::Title { "MrMountainMan Github" }
+        Router::<Route> {}
     }
 }
 
+//home page
 #[component]
-pub fn Hero() -> Element {
+fn Home() -> Element {
     rsx! {
-        div {
-            id: "hero",
-            img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.7/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
-            }
-        }
+        //document::Link { rel: "icon", href: DEFAULT_ICON }
+        //document::Title { "MrMountainMan Github" }
+
+        h2 { "Home Page!" }
+
+        //Link { to: Route::Monaco2CalcMain {}, "Monaco 2 Score Calculator" }
+        br {}
+        //Link { to: Route::Payday3Stats {}, "Payday 3 Stats" }
     }
 }
